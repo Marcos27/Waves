13b178d853d9fea94ceaec196658c2faf347faca
Brigette Morando <bibimorando13@yahoo.com>
2016-05-24 15:50:47 -0700

    finished adding devise

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,3 +1,12 @@
 (function() {
-  angular.module('wavesApp', ['ui.router', 'ngResource'])
+  angular.module('wavesApp', ['ui.router', 'ngResource', 'ng-token-auth'])
+    .config(function($authProvider) {
+          $authProvider.configure({
+              apiUrl: 'http://localhost:3000',
+              authProviderPaths: {
+                spotify: '/auth/spotify'
+              }
+          });
+        });
 })();
+
--- frontend/js/music/musicControllers.js
+++ frontend/js/music/musicControllers.js
@@ -3,14 +3,27 @@
     .controller("MusicListController", MusicListController)
 
 
-    MusicListController.$inject = ['MusicResource']
+    MusicListController.$inject = ['MusicResource', '$auth']
 
-    function MusicListController(MusicResource) {
+
+    function MusicListController(MusicResource, $auth) {
       var vm = this;
       vm.musics = [];
+
+      vm.spotifyLogin = spotifyLogin;
 
       MusicResource.query().$promise.then(function(musics) {
         vm.musics = musics;
       })
+
+      function spotifyLogin(){
+        $auth.authenticate('spotify')
+          .then(function(resp) {
+            console.log(resp)
+          })
+          .catch(function(resp) {
+            console.log('errors: ', resp)
+          })
+      }
     }
 })();
