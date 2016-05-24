afe029fa390ed46cdd36d9507e6ea90f588755b7
Marcos Felix <marcosfelix299@yahoo.com>
2016-05-23 17:55:44 -0700

    created a model for music and add frontend and backend now render JSON on index page

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -31,5 +31,13 @@ module Backend
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :put, :post, :delete, :options]
+      end
+    end
   end
 end
